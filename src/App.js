6afdafc33001c739de3c7beaa03209b06f6443fb
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-01 18:47:22 -0400

    Basic navbar and header, along with colors done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './css/index.css';
-import NavBar from './components/Navbar';
-import Header from './components/Header';
-import AboutMe from './components/AboutMe';
-import MajorProjects from './components/MajorProjects';
-import MinorProjects from './components/MinorProjects';
-import WorkWithMe from './components/WorkWithMe';
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import './css/index.css';
+// import NavBar from './components/Navbar';
+// import Header from './components/Header';
+// import AboutMe from './components/AboutMe';
+// import MajorProjects from './components/MajorProjects';
+// import MinorProjects from './components/MinorProjects';
+// import WorkWithMe from './components/WorkWithMe';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <Header />
-        <AboutMe />
-        <MajorProjects />
-        <MinorProjects />
-        <WorkWithMe />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <NavBar />
+//         <Header />
+//         <AboutMe />
+//         <MajorProjects />
+//         <MinorProjects />
+//         <WorkWithMe />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
